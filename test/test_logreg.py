f26ad60951292e27be7d6c3f5296a332611f5724
Brenda Miao <brenda.miao@ucsf.edu>
2022-02-14 15:43:53 -0800

    detailed unit test instructions

--- test/test_logreg.py
+++ test/test_logreg.py
@@ -2,10 +2,25 @@
 Write your logreg unit tests here. Some examples of tests we will be looking for include:
 * check that fit appropriately trains model & weights get updated
 * check that predict is working
+
+More details on potential tests below, these are not exhaustive
 """
 
 def test_updates():
+	"""
+
+	"""
+	# Check that your gradient is being calculated correctly
+	
+	# Check that your loss function is correct and that 
+	# you have reasonable losses at the end of training
+
 	pass
 
 def test_predict():
+	# Check that self.W is being updated as expected
+	# and produces reasonable estimates for NSCLC classification
+
+	# Check accuracy of model after training
+
 	pass
