dafc9b7982797de75c34ba91a51cd54f95014a09
williams-jacquelinec <williams.jacquelinec@gmail.com>
2022-02-23 15:37:47 -0800

    pushing changes

--- regression/__init__.py
+++ regression/__init__.py
@@ -2,3 +2,8 @@
 BMI203: Biocomputing algorithms Winter 2022
 Assignment 6: Logistic regression
 """
+
+from .logreg import BaseRegressor, LogisticRegression
+from .utils import loadDataset
+
+__version__ = '0.1.0'
